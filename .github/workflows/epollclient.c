efc1a0947b91ca4b1bec01ea37665c621c3d4d57
tkfka8989 <68724041+tkfka8989@users.noreply.github.com>
2022-03-16 17:39:24 +0900

    Update epollclient.c

--- .github/workflows/epollclient.c
+++ .github/workflows/epollclient.c
@@ -1,96 +1,103 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <string.h>
 #include <arpa/inet.h>
- 
-#define MAXLINE 1024
-char *EXIT_STRING = "exit";
- 
-int main(int argc, char **argv)
+#include <sys/socket.h>
+#include <pthread.h>
+
+#define MAXLINE 1000
+#define BUF_SIZE 1000
+#define NAME_SIZE 2000
+	
+void * send_msg(void * arg);
+void * recv_msg(void * arg);
+void error_handling(char * msg);
+	
+char name[NAME_SIZE]="[DEFAULT]";
+char msg[BUF_SIZE];
+char Num[20];
+int main(int argc, char *argv[])
 {
-  
-  int socketfd;
-  struct sockaddr_in sockaddr;
-  int socklen;
-  fd_set readfds, oldfds;
-  int maxfd;
-  int fd_num;
-  char buf[MAXLINE];
-  int readn, writen;
- 
-  if(argc != 3)
-  {
-      printf("Usage : %s [ip address] [port]\n", argv[0]);
-      return 1;
-  }
-  if( (socketfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
-  {
-      return 1;
-  }
- 
-  sockaddr.sin_family = AF_INET;
-  sockaddr.sin_addr.s_addr = inet_addr(argv[1]);
-  sockaddr.sin_port = htons(atoi(argv[2]));
- 
-  socklen = sizeof(sockaddr);
-  
-  if(connect(socketfd, (struct sockaddr *)&sockaddr, socklen) == -1)
-  {        perror("connect error");
-         return 1;
-  }
-  
-  // readfd set 0 
-  FD_ZERO(&readfds);
-  // socketfd에 해당하는 readfds의 비트를 1로 한다.
-  FD_SET(socketfd, &readfds);
-  // 0번째 인자를 1로 set한다
-  FD_SET(0, &readfds);
- 
-  maxfd = socketfd;
-  oldfds = readfds;
- 
-  while(1)
-  {
-      readfds = oldfds;
-      fd_num = select(maxfd + 1, &readfds, NULL, NULL, NULL);
-      if(FD_ISSET(socketfd, &readfds))        // 초기 연결인지 아닌지 검사를 한다.
-      {
-          memset(buf, 0x00, MAXLINE);
-          readn = read(socketfd, buf, MAXLINE);
-          if(readn <= 0)
-          {
-              return 1;
-          }
-          writen = write(1, buf, readn);
-          if(writen != readn)
-          {
-              return 1;
-          }
-      }
-      if(FD_ISSET(0, &readfds))                // raedfds의 0번째 인자가 set?
-      {
-          memset(buf, 0x00, MAXLINE);
-          readn = read(0, buf, MAXLINE);
-          printf("> %s", buf);
-          if(readn <= 0)
-          {
-              return 1;
-          }
-          writen = write(socketfd, buf, readn);
-		  if (strstr(buf, EXIT_STRING) != NULL) {
-				puts("Good bye.");
-				close(socketfd);
+	
+	char bufmsg[MAXLINE];
+	int sock;
+	struct sockaddr_in serv_addr;
+	pthread_t snd_thread, rcv_thread;
+	void * thread_return;
+	if(argc!=4) {
+		printf("Usage : %s <IP> <port> <name>\n", argv[0]);
+		exit(1);
+	 }
+	
+	
+	sock=socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&serv_addr, 0, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+	  
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))==-1)
+		error_handling("connect() error");
+
+	write(sock, argv[3], sizeof(argv[3]));
+	memset(bufmsg, 0, sizeof(bufmsg));
+	read(sock, bufmsg, MAXLINE);
+	strncpy(Num, bufmsg, sizeof(bufmsg));
+	snprintf(name, 130, "[%s, %s]", Num, argv[3]);
+
+	pthread_create(&snd_thread, NULL, send_msg, (void*)&sock);
+	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&sock);
+	pthread_join(snd_thread, &thread_return);
+	pthread_join(rcv_thread, &thread_return);
+	close(sock);  
+	return 0;
+}
+	
+void * send_msg(void * arg)   // send thread main
+{
+	int sock=*((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE];
+	
+	while(1) 
+	{
+		memset(name_msg, 0 , sizeof(name_msg));
+		sleep(2);
+		printf("%s",name);
+		if(fgets(msg, BUF_SIZE, stdin)){
+			if(!strcmp(msg,"exit\n")||!strcmp(msg,"EXIT\n")) 
+			{
+				close(sock);
 				exit(0);
-		  }
-          if(readn != writen)
-          {
-              return 1;
-          }
-      }
-  }
+			}
+			snprintf(name_msg, 3000, "%s %s", name, msg);
+			write(sock, name_msg, strlen(name_msg));
+		}
+		
+	}
+	return NULL;
+}
+	
+void * recv_msg(void * arg)   // read thread main
+{
+	int sock=*((int*)arg);
+	char name_msg[NAME_SIZE+BUF_SIZE];
+	int str_len;
+	while(1)
+	{
+		str_len=read(sock, name_msg, NAME_SIZE+BUF_SIZE-1);
+		if(str_len==-1) 
+			return (void*)-1;
+		name_msg[str_len]=0;
+		fputs(name_msg, stdout);
+	}
+	return NULL;
+}
+	
+void error_handling(char *msg)
+{
+	fputs(msg, stderr);
+	fputc('\n', stderr);
+	exit(1);
 }
